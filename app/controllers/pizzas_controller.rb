2cd5ae7e768bed6185c8bce912bf6ba23106f0bc
Aymes <aysikh@MacBook-Pro.fios-router.home>
2020-09-22 12:48:42 -0400

    Trying to work on edit form

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -10,13 +10,23 @@ class PizzasController < ApplicationController
 
     def new
         @pizza = Pizza.new
+        @restaurants = Restaurant.all
     end
 
     def create
-
         name = params["pizza"]["name"]
         vegetarian = params["pizza"]["vegetarian"]
-            pizza = Pizza.create(name: name, vegetarian: vegetarian)
+        restaurant = params["pizza"]["restaurant"]
+            pizza = Pizza.create(name: name, vegetarian: vegetarian, restaurant: Restaurant.find(params["pizza"]["restaurant"]))
+            # if params["pizza"]["topping1"] != ""
+            #     pizza.toppings << Topping.find_or_create_by(name: params["pizza"]["topping1"])
+            # end
+            # if params["pizza"]["topping2"] != ""
+            #     pizza.toppings << Topping.find_or_create_by(name: params["pizza"]["topping2"])
+            # end
+            # if params["pizza"]["topping3"] != ""
+            #     pizza.toppings << Topping.find_or_create_by(name: params["pizza"]["topping3"])
+            # end
             toppings = params["pizza"]["topping"]
             toppings.each do |topping|
                 x = Topping.find_or_create_by(name: topping)
@@ -26,14 +36,48 @@ class PizzasController < ApplicationController
     end
 
     def edit
+        @pizza = Pizza.find(params[:id])
+        @restaurants = Restaurant.all
+        render :edit
     end
 
     def update
+        @pizza = Pizza.update(params[:id])
+        redirect_to pizzas_path
     end
 
     def destroy
         @pizza = Pizza.destroy(params[:id])
-        redirect_to pizza_path
+        redirect_to pizzas_path
     end
 
+    # private
+
+    # def pizza_params
+    #   params.require(:pizza).permit(:name,:vegetarian, :restaurant_id)
+    # end
+
 end
+
+
+
+# <%= form_for @pizza do |f| %>
+#     <%= f.label :name, "Pizza Name:" %>
+#     <%= f.text_field :name %>
+#     <br><br>
+#     <%= f.label :topping1 %>
+#     <%= f.text_field :toppings %>
+#     <br><br>
+#     <%= f.label :topping2 %>
+#     <%= f.text_field :toppings %>
+#     <br><br>
+#     <%= f.label :topping3 %>
+#     <%= f.text_field :toppings %>
+#     <br><br>
+#     <%= f.label :vegetarian, "Vegetarian?" %>
+#     <%= f.select :vegetarian, [true, false] %>
+#     <br><br>
+#     <%= f.collection_select(:restaurant_id, Restaurant.all, :id, :name) %>
+#     <br><br>
+#     <%= f.submit button_text %>
+#   <% end %>
